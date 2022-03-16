b16a3b5491878799d685e05e102d757e12343b60
yoratorad1337 <yoratorad0@gmail.com>
2022-03-17 02:08:13 +0300

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*main - check the code
+* Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,10 @@
 /* 0-putchar.c */
 int _putchar(char c);
 
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+
 
 
 
