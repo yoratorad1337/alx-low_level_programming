194c4085fc7802699ef93c89512baa1bf4e809ea
yoratorad1337 <yoratorad0@gmail.com>
2022-03-19 01:45:48 +0300

    for loop

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+    int i;
+
+    i =  3;
+
+    for (j = 0; i > j; j++)
+    {
+
+    	if (a > b && b > c)
+    	{
+        	largest = a;
+    	}
+    	else if (b > a && a > c)
+    	{
+        	largest = b;
+    	}
+    	else
+    	{
+        	largest = c;
+    	}
+
+    }
+    return (largest);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,7 @@
 
 void positive_or_negative(int 1);
 
+int largest_number(int a, int b, int c);
+
+
 #endif /* MAIN_H */
