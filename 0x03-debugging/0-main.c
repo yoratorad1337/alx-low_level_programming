d352f4aab5d338967803fd1e773db1a747d0a223
yoratorad1337 <yoratorad0@gmail.com>
2022-03-19 00:11:32 +0300

    0x03

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negarive
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	(i==0)?printf("%d is zero",i : positive_or_negative(i);
+
+	return (0);
+}
