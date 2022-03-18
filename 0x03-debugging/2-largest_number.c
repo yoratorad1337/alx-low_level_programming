2fe361c204b9e5616b1761264b742b213dcb86c8
yoratorad1337 <yoratorad0@gmail.com>
2022-03-19 01:58:47 +0300

    keep trying

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,27 +11,20 @@
 int largest_number(int a, int b, int c)
 {
     int largest;
-    int i, j;
-
-    i =  3;
-
-    for (j = 0; i = j; j++)
+    
+    if (a >= b && b >= c)
     {
-
-    	if (a >= b && b >= c)
-    	{
-        	largest = a;
-    	}
-    	else if (b >= a && a >= c)
-    	{
-        	largest = b;
-    	}
-    	else
-    	{
-        	largest = c;
-    	}
-
+       	largest = a;
     }
+    else if (b >= a && a >= c)
+    {
+       	largest = b;
+    }
+    else
+    {
+       	largest = c;
+    }
+
     return (largest);
 }
 
