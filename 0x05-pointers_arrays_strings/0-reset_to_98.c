96a833c5574ec9b21675d06b45449284e87d0ce5
yoratorad1337 <yoratorad0@gmail.com>
2022-03-27 18:33:52 +0300

    keep trying

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,7 +5,6 @@
  * the pointer points to 98
  * @n: pointer to the variable to update
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 #0.98 Battery
 void reset_to_98(int *n);
 
