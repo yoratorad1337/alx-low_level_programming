ea40333fc700a3100ef846db34e96246b804a2a3
yoratorad1337 <yoratorad0@gmail.com>
2022-03-29 01:51:32 +0300

    main.h typo

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 /* 0.98 Battery */
 void reset_to_98(int *n);
 
-/*don't swap horses in crossing a stream
+/*don't swap horses in crossing a stream*/
 void swap_int(int *a, int *b);
 
 #endif
