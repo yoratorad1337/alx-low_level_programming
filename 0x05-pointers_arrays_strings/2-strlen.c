fac630dbfe0a1906574a4ea986694c0b1bff61cd
yoratorad1337 <yoratorad0@gmail.com>
2022-03-29 02:24:48 +0300

    betty style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
 
-/*
-
-
-
-
-*/
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
 	int num;
