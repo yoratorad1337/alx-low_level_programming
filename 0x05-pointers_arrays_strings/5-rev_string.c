e7bb71f0a238ed5dc6f186e93487bd1ab57e620d
yoratorad1337 <yoratorad0@gmail.com>
2022-04-03 17:12:38 +0300

    returning rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+void puts2(char *str)
+{
+	int i, j, num;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	num = i;
+
+	for (j = num - 1; j >= 0; j--)
+	{
+		str[j];
+	}
+
+	_putchar('\n');
+}
