f7a4ae5308027f473beac7352400244ee7d26960
yoratorad1337 <yoratorad0@gmail.com>
2022-04-03 16:54:13 +0300

    while with for loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,11 +4,19 @@
 
 void print_rev(char *s)
 {
-	int num = strlen(s);
-	int i = 0;
-	while (num >= 0)
+	int i, j, len;
+	i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 		{
-			_putchar("%c",s[i]);
+			_putchar(s[i]);
 		}
 	_putchar("\n");
 
