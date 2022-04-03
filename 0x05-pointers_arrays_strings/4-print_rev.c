30b987af6f94489b46c0f9a56ff54aa9bffef55b
yoratorad1337 <yoratorad0@gmail.com>
2022-04-03 17:04:08 +0300

    betty style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,7 @@
 void print_rev(char *s)
 {
 	int i, j, len;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -17,9 +18,10 @@ void print_rev(char *s)
 	len = i;
 
 	for (j = len - 1; j >= 0; j--)
-		{
-			_putchar(s[j]);
-		}
+	{
+		_putchar(s[j]);
+	}
+
 	_putchar('\n');
 
 }
