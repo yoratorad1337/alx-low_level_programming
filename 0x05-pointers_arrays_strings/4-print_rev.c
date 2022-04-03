041582f7b9759154fe73cbcdfd3bdc8641421155
yoratorad1337 <yoratorad0@gmail.com>
2022-04-03 17:01:14 +0300

    typo erro

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-
-
+/**
+* print_rev - prints a string in reverse, followed by a new line
+* @s: string to be printed
+*/
 void print_rev(char *s)
 {
 	int i, j, len;
 	i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -16,7 +18,7 @@ void print_rev(char *s)
 
 	for (j = len - 1; j >= 0; j--)
 		{
-			_putchar(s[i]);
+			_putchar(s[j]);
 		}
 	_putchar('\n');
 
