f228adfeea2acb6f4e6c6c4992c52b23a21b45f5
yoratorad1337 <yoratorad0@gmail.com>
2022-04-03 16:38:16 +0300

    print in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 
+
 void print_rev(char *s);
 {
 	int num = strlen(s);
 	for (int i = num; i >=0; i--);
 		{
-			_putchar(s[i]);
+			_putchar("%c",s[i]);
 		}
 	_putchar("\n");
 
