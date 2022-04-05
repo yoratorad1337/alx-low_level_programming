a2ddbea232ffa42e974f4fc4723d3775e881eb1f
yoratorad1337 <yoratorad0@gmail.com>
2022-04-06 02:15:01 +0300

    low level programming

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d",a[i]);
-		if (i + 1 != n)
+		if (i == 0)
 		{
-			printf(", ");
+			printf("%d",a[i]);
 		}
+		else
+		{
+			printf(", %d",a[i]);
+		}
+		printf("\n");
 	}
 }
