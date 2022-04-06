8155e5fc49d97ed6a539b6ad6bbaf06f188ff3d7
yoratorad1337 <yoratorad0@gmail.com>
2022-04-07 01:43:32 +0300

    mandatory

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,55 +1,39 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
+ * numchecker - verifies if string has only numbers
+ * @s: string to check
+ * Description: return 1 if only numbers in string, 0 if not
+ * Return: 1 if only numbers, 0 if not
+ **/
+
+int numchecker(char *s)
 {
-	int count = 0;
+	int i;
 
-	while (i != 0)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-			else if (i % 2 == 0)
-		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
-		}
-		else
-			i -= 1;
-
-		count++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 	}
-
-	return (count);
+	return (1);
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
- */
+ * main - find least amount of coins needed for change
+ * @argc: number of parameters entered
+ * @argv: strings entered including file name
+ * Description: prints the minimum number of coins to make change
+ * Return: zero
+ **/
+
 int main(int argc, char *argv[])
 {
-	int i, coin;
+	int change, coins;
+	char *input = argv[1];
 
-	coin = 0;
+	change = coins = 0;
 
 	if (argc != 2)
 	{
@@ -57,17 +41,31 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	i = atoi(argv[1]);
+	change = atoi(input);
 
-	if (i < 0)
-		printf("0\n");
-
-	else
+	if (change >= 0 && numchecker(input) == 0)
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		printf("Error\n");
+		return (1);
 	}
-
+	if (change < 0 && numchecker(&input[1]) == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (change < 0)
+	{
+		change = 0;
+	}
+	coins = change / 25;
+	change = change % 25;
+	coins = coins + change / 10;
+	change = change % 10;
+	coins = coins + change / 5;
+	change = change % 5;
+	coins = coins + change / 2;
+	change = change % 2;
+	coins += change;
+	printf("%d\n", coins);
 	return (0);
 }
