892ec2881172ffb4b2cfb3531617eed251ba58ba
yoratorad1337 <yoratorad0@gmail.com>
2022-04-09 23:30:53 +0300

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,13 +7,20 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	char *tmp = dest;
+	char *tmp = dest, *tmp1;
 
 	while (*src !='\0')
 	{
 	*src = *dest;
 	dest++;
 	src++;
+	}
+	*a = *tmp1;
+	while (*tmp != '\0');
+	{
+		*tmp1 = *tmp;
+		tmp1++;
+		tmp++;
 	}
 
 	*dest = '\0';
