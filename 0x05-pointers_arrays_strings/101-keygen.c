3d056ed6524116c3fb06cc6faa651f6614457e3a
yoratorad1337 <yoratorad0@gmail.com>
2022-04-12 17:41:27 +0300

    Don't hate the hacker

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+	return (0);
+}
